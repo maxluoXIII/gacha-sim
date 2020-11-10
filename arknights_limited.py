d989e851e5c8972cfaad4ee09278d74981e9b959
Maxiwell Luo <maxluoXIII@gmail.com>
2020-11-10 00:25:50 -0600

    Add graphs and reduce number of simulations

--- arknights_limited.py
+++ arknights_limited.py
@@ -1,7 +1,9 @@
 import random
+import matplotlib.pyplot as plt
 
 results = {k: 0 for k in range(1, 301)}
-num_simulations = 1000000
+num_simulations = 10000
+saved_rolls = 104
 num_six_star_rate_ups = 2
 num_targeted_rate_ups = 1
 target_ratio = num_targeted_rate_ups / num_six_star_rate_ups
@@ -24,8 +26,26 @@ for sim_num in range(num_simulations):
 sorted_results = {k: v/num_simulations for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)}
 print(sorted_results)
 
-saved_rolls = 91
-cum_prob = 0
-for i in range(1, saved_rolls+1):
-    cum_prob += results[i] / num_simulations
-print(cum_prob)
+plt.figure(1)
+plt.subplot(211)
+plt.bar(sorted_results.keys(), sorted_results.values(), 1.0)
+plt.title("Probability Distribution")
+plt.xlabel("Number of rolls")
+plt.ylabel("Probability")
+
+cum_probs = [0] * 301 # valid indices [0, 300]
+for i in range(1, 300+1):
+    cum_probs[i] = cum_probs[i-1] + results[i] / num_simulations
+print(cum_probs[saved_rolls])
+
+plt.subplot(212)
+plt.bar(range(0, 301), cum_probs, width=1.0)
+plt.title("Cumulative Probability Distribution")
+plt.xlabel("Number of rolls")
+plt.ylabel("Cumulative probability")
+plt.axhline(cum_probs[saved_rolls], color="red")
+plt.axvline(saved_rolls, color="red")
+plt.annotate(f"{cum_probs[saved_rolls]:.2}", xy=(saved_rolls, cum_probs[saved_rolls]), xytext=(saved_rolls - 25, cum_probs[saved_rolls] + 0.1))
+plt.tight_layout()
+plt.savefig("arknights_limited.png")
+plt.show()
